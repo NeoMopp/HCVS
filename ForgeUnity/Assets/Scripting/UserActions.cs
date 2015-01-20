e90f26263ee45dbdf2ee35fd47217a1f11a7feb3
NeoMopp <willzsawyer@gmail.com>
2015-01-20 14:52:22 +0000

    added a lot of things to the system
    Added forge 1.1 and some other things

---
+++ ForgeUnity/Assets/Scripting/UserActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserActions : MonoBehaviour {
+    Animator animationController; 
+	// Use this for initialization
+	void Start () {
+        animationController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            animationController.SetTrigger("UserAction");
+
+        if (Input.GetKeyDown("e"))
+            
+        animationController.SetBool("Holding", !animationController.GetBool("Holding"));
+	}
+
+    GameObject getObjectView(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit ray;
+        Vector3 target = position + Camera.main.transform.forward * range;
+        if (Physics.Linecast(position, target, out ray))
+            return ray.collider.gameObject;
+        return null;
+    }
+}
