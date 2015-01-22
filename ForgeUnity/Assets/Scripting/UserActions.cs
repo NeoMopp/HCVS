94768ad147fb56798de086e6dbf703891b434c26
NeoMopp <willzsawyer@gmail.com>
2015-01-22 17:20:46 +0000

    Added GUI to the main "instance"

--- ForgeUnity/Assets/Scripting/UserActions.cs
+++ ForgeUnity/Assets/Scripting/UserActions.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class UserActions : MonoBehaviour {
-    Animator animationController; 
+    Animator animationController;
+    public GameObject obj1, obj2, obj3;
+
+
 	// Use this for initialization
 	void Start () {
         animationController = GetComponent<Animator>();
@@ -14,8 +17,28 @@ public class UserActions : MonoBehaviour {
             animationController.SetTrigger("UserAction");
 
         if (Input.GetKeyDown("e"))
-            
-        animationController.SetBool("Holding", !animationController.GetBool("Holding"));
+            animationController.SetBool("Holding", !animationController.GetBool("Holding"));
+
+        if (Input.GetKeyDown("q"))
+        {
+            animationController.SetBool("Holding", true);
+            changeObject();
+        }
+        if (Input.GetKeyDown("1"))
+        {
+            animationController.SetBool("Holding", true);
+            changeObject(1);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            animationController.SetBool("Holding", true);
+            changeObject(2);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            animationController.SetBool("Holding", true);
+            changeObject(3);
+        }
 	}
 
     GameObject getObjectView(float range)
@@ -26,5 +49,50 @@ public class UserActions : MonoBehaviour {
         if (Physics.Linecast(position, target, out ray))
             return ray.collider.gameObject;
         return null;
+    }
+
+    void changeObject()
+    {
+        if (obj1.activeSelf)
+        {
+            obj1.SetActive(false);
+            obj2.SetActive(true);
+            //animationController.SetBool("Holding", true);
+        }
+
+        else
+        {
+            obj2.SetActive(false);
+            obj1.SetActive(true);
+            //animationController.SetBool("Holding", true);
+        }
+    }
+
+    void changeObject(int num)
+    {
+        switch (num) { 
+            case 1:
+                obj1.SetActive(true);
+                obj2.SetActive(false);
+                obj3.SetActive(false);
+                break;
+            case 2:
+                obj1.SetActive(false);
+                obj2.SetActive(true);
+                obj3.SetActive(false);
+                break;
+            case 3:
+                obj1.SetActive(false);
+                obj2.SetActive(false);
+                obj3.SetActive(true);
+                break;
+            default:
+                obj1.SetActive(false);
+                obj2.SetActive(false);
+                obj3.SetActive(false);
+                break;
+
+        }
+
     }
 }
