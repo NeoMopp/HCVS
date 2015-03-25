6503627d9018183378bea05ff27501ca8125e53d
NeoMopp <willzsawyer@gmail.com>
2015-03-25 10:11:53 +0000

    Controls for UI

--- ForgeUnity/Assets/Scripting/AnimatorControls.cs
+++ ForgeUnity/Assets/Scripting/AnimatorControls.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
 
 public class AnimatorControls : MonoBehaviour {
 
     public Animator MainMenu, SubMenu;
+    public GameObject myo = null;
+    private Pose _lastPose = Pose.Unknown;
+    public GameObject PrintButton = null, GuideButton = null, ResetButton = null, HorseshoeBut = null, HookBut = null, FreeBut = null;
 
 	// Use this for initialization
 	void Start () {
@@ -13,41 +22,117 @@ public class AnimatorControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        
+        //ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        //if (thalmicMyo.pose != _lastPose)
+        //{
+        //    _lastPose = thalmicMyo.pose;
+        //    
+        //    //This pose enables/diables the menu
+        //    if (thalmicMyo.pose == Pose.DoubleTap && MainMenu.GetBool("IsHidden"))
+        //    {
+        //        openMainMenu();
+        //        ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        //    }
+
+        //    if (thalmicMyo.pose == Pose.WaveIn && MainMenu.GetBool("IsHidden")== true)
+        //    {
+        //        ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        //    }
+
+        //    if (thalmicMyo.pose == Pose.WaveOut && MainMenu.GetBool("IsHidden") == true)
+        //    {
+        //        ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        //    }
+
+        //    if (thalmicMyo.pose == Pose.Fist && MainMenu.GetBool("IsHidden") == true)
+        //    {
+        //        ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        //    }
+
+        //    if (thalmicMyo.pose == Pose.FingersSpread && MainMenu.GetBool("IsHidden") == true)
+        //    {
+        //        ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        //    }
+        //}
+        
+        if (Input.GetKeyUp(KeyCode.Escape) && MainMenu.GetBool("IsHidden"))
         {
             Debug.LogError("Esc");
-            {
-                openMainMenu();
-                
-            }
+            openMainMenu();              
         }
-	
+
+        else if (Input.GetKeyUp(KeyCode.Escape) && !MainMenu.GetBool("IsHidden"))
+        {
+            Debug.LogError("Esc");
+            closeSubMenu();
+            closeMainMenu();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Backspace) && MainMenu.GetBool("StateChange") && !SubMenu.GetBool("IsHidden"))
+        {
+            Debug.LogError("Backspace");
+            closeSubMenu();         
+        }
 	}
 
+    //Shows the main menu
     public void openMainMenu()
     {
-        MainMenu.SetBool("IsHidden", false);
+        MainMenu.SetBool("IsHidden", false); 
+        EventSystem.current.SetSelectedGameObject(PrintButton);
     }
 
+    //Hides the main menu
     public void closeMainMenu()
     {
         MainMenu.SetBool("IsHidden", true);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
+    //Shows the sub menu while hiding the main menu
     public void openSubMenu()
     {
         MainMenu.SetBool("StateChange", true);
         SubMenu.SetBool("IsHidden", false);
+        EventSystem.current.SetSelectedGameObject(HorseshoeBut);
     }
 
+    //Hides the sub menu while showing the main menu
     public void closeSubMenu()
     {
         SubMenu.SetBool("IsHidden", true);
         MainMenu.SetBool("StateChange", false);
+        EventSystem.current.SetSelectedGameObject(PrintButton);
     }
 
+    //Invert the Mainmenu
     public void switchMenu()
     {
         MainMenu.SetBool("IsHidden", !MainMenu.GetBool("IsHidden"));
     }
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
+
+    //On guide button press
+    public void GuidePress()
+    {
+        openSubMenu();
+        
+    }
 }
+
+
