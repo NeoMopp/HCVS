aed867b6db3e0894d827ea63d15cae23566b40ce
NeoMopp <willzsawyer@gmail.com>
2015-04-14 17:19:10 +0100

    Added the ability to change "Maleability" when in the fire.

---
+++ ForgeUnity/Assets/Scripting/FireCollisions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCollisions : MonoBehaviour {
+
+    Deformable deform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log("Mesh Entered the trigger");
+        deform = other.GetComponent<Deformable>();
+    }
+
+    void OnTriggerStay(Collider other){
+        Debug.Log("Mesh is within the trigger");
+        deform.Hardness = deform.Hardness += 0.1f;
+        Debug.Log(deform.Hardness);
+    }
+
+    void OnTriggerExit(Collider other){
+        Debug.Log("Mesh Exited the trigger");
+
+    }
+
+}
